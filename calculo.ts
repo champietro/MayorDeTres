73ded03b1b618c1c08e80170a16374244bca6f23
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-25 16:50:51 -0300

    fgdfg

--- calculo.ts
+++ calculo.ts
@@ -1,12 +1,12 @@
 let btnEnv = document.getElementById("btnEnviar");
 
 btnEnv.addEventListener("click", () => {
-  let numeroMax: Number= 0;  
-  for (let veces : Number = 1; veces <= 4; veces++ ) {
-      ingresado = Number(prompt("Ingrese un número: "));
-      if ingresado <= numeroMax{
-        numeroMax= ingresado;
-      }
+  let numeroMax: Number = 0;
+  for (let veces: Number = 1; veces <= 4; veces++) {
+    ingresado = Number(prompt("Ingrese un número: "));
+    if (ingresado <= numeroMax) {
+      numeroMax = ingresado;
     }
-    console.log("El número mayor es: ", numeroMax);
+  }
+  console.log("El número mayor es: ", numeroMax);
 });
