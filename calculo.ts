a992b6629d4c62de9ccef20937c18101e9452b9a
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-25 16:42:00 -0300

    rte

--- calculo.ts
+++ calculo.ts
@@ -1,9 +1,9 @@
 let btnEnv = document.getElementById("btnEnviar");
 
 btnEnv.addEventListener("click", () => {
-  let numeroMax: number;  
+  let numeroMax: number= 0;  
   let ingresado: number;
-  for (let veces : number = 1; veces <= 4; contador++ ) {
+  for (let veces : number = 1; veces <= 4; veces++ ) {
      ingresado = prompt("Ingrese un número:");
       if ingresado <= numeroMax{
         numeroMax= ingresado;
