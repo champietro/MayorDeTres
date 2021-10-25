725b0313c52f9d89048776d2b37625dba7d63eb6
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-25 16:59:02 -0300

    final

--- calculo.ts
+++ calculo.ts
@@ -1,10 +1,10 @@
 let btnEnv = document.getElementById("btnEnviar");
 
 btnEnv.addEventListener("click", () => {
-  let numeroMax: Number = 0;
-  for (let veces: Number = 1; veces <= 4; veces++) {
-    ingresado = Number(prompt("Ingrese un número: "));
-    if (ingresado <= numeroMax) {
+  let numeroMax = Number(prompt("Ingrese un número: "));
+  for (let veces: Number = 1; veces <= 2; veces++) {
+    let ingresado = Number(prompt("Ingrese un número: "));
+    if (ingresado >= numeroMax) {
       numeroMax = ingresado;
     }
   }
